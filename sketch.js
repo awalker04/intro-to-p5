4445d919d4aff151450d766f2374844662d4e295
Amaiya Walker <awalker@wlapcs.org>
2017-03-28 17:38:45 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,44 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+function draw(){
+    line(35,80,300,200);
+    
+    stroke(255.255,255);
+    fill(255,0,0);
+    rect(290,100,250,250);
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);  
+}
+
+function draw(x,y){
+    line(35,80,300,200);
+    
+    stroke(255.255,255);
+    fill(255,0,0);
+    rect(290,100,250,250);
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);  
+    
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,250,255);
+    ellipse(50,250,80,80);
+    line( 35,80,400,200);
+    
+    line(35,80,40,250);
+    stroke(255,255,270);
+    fill(220,25,250);
+    ellipse(70,250,80,80);
+    rect(200,100,100,100);
+     line( -200,215,3050,200);
+     line( 500,10,500,300);
+}
